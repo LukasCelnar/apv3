7d229cc39f52ba50b41b6d5792c5ca2882b75c46
Drizz159 <celnar.lukas@gmail.com>
2021-06-02 12:01:26 +0200

    Making Info dynamic with the entered url

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Info.scss';
+import {useLocation} from 'react-router-dom';
+
+/* const code = useLocation().pathname.split('/')[1] */
+
+//import infoData from '/assets/B60804/infoData.json'
 
 const Info = () => {
+    const [infoData, setInfoData] = useState(null)
+
+    const code = useLocation().pathname.split('/')[1];
+
+    useEffect(() => {
+
+        fetch(`/assets/${code}/infoData.json`)
+            .then(res => res.json())
+            .then(data => setInfoData(data))
+            .catch(err => console.log(err))
+    }, [infoData, code])
+
+    const renderSpecsContent = () => {
+        const specsContent = {keys: [], values: []}
+        for (const key in infoData.specs.content) {
+            specsContent.keys.push(<div key={key}>{key}</div>);
+            specsContent.values.push(<div key={key}>{infoData.specs.content[key]}</div>);
+        }
+        return specsContent;
+    }
+
+    if (!infoData) {
+        return null
+    }
+
     return (
         <div className='info' id='info'>
-           <div className='info__header'>PALÁC TRNITÁ - STŘEŠNÍ BYT (B6/08.04)</div>
+           <div className='info__header'>{infoData.header}</div>
            <div className='info__content'>
                 <img className='info__img info__img-left' alt='main-picutre1' src='/imgs/main1.jpg'/>
                 <div className='info__specs'>
-                    <div className='info__specs-header'>4+kk</div>
+                    <div className='info__specs-header'>{infoData.specs.header}</div>
                     <div className='info__specs-content' style={{display: 'flex'}}>
                         <div className='info__specs-content-left-column' style={{width: '65%'}}>
-                            <div>Podlaží</div>
-                            <div>Užitná plocha</div>
-                            <div>Celková plocha</div>
-                            <div>Balkon</div>
-                            <div>Terasa</div>
-                            <div>Sklepní kóje</div>
-                            <div>Garážové stání</div>
-                            <div>K nastěhování</div>
+                            {renderSpecsContent().keys}
                         </div>
                         <div className='info__specs-content-right-column' style={{width: '35%'}}>
-                            <div>8.NP a 9.NP</div>
-                            <div>106,6 m²</div>
-                            <div>115,5 m²</div>
-                            <div>20,3 m²</div>
-                            <div>31,4 m²</div>
-                            <div>Ano</div>
-                            <div>Ano 2x</div>
-                            <div>Podzim 2022</div>
+                            {renderSpecsContent().values}
                         </div>
                     </div>
-                    <div className='info__specs-footer'>12 999 000 Kč</div>
+                    <div className='info__specs-footer'>{infoData.specs.price}</div>
                 </div>
                 <img className='info__img info__img-right' alt='main-picture2' src='/imgs/main2.jpg'/>
            </div>
